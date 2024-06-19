4e8c6ad0e55cea0fdb62dc906d7205d23a52c4c0
jassuwu <karthickprnv@gmail.com>
2024-06-19 19:29:49 +0530

    feat: rm the samples and write homepage

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,9 @@
 
 export const SITE_TITLE = 'jass';
 export const SITE_DESCRIPTION = 'writing code no one asked for';
+
+export const GITHUB_URL = 'https://www.github.com/jassuwu'
+export const LINKEDIN_URL = 'https://www.linkedin.com/in/kprnv'
+export const DISCORD_USERNAME = 'jassuwu'
+export const DISCORD_USER_URL = 'https://discordapp.com/users/421489076978450445'
+export const EMAIL_ID = 'mailto:karthickprnv@gmail.com'
